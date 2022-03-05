19c741b85ba7169a1a415bcb53e9025d3ae5be94
maureen kagochi <kagochimaureen99@gmail.com>
2022-03-05 17:38:36 -0500

    modify file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var akan = function(y, m, d, g) {
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let akan = function(y, m, d, g) {
+    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    let maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var d = new Date(y, --m, d);
     if (g === "Female") {
         return d && femaleNames[d.getDay()];
@@ -16,8 +16,8 @@ $( document ).ready(function() {
         var y = parseInt($("#year").val());
         var m = parseInt($("#month").val());
         var d = parseInt($("#date").val());
-        var g = $("input:radio[name=gender]:checked").val();
-        var result = akan(y, m, d, g);
+        var g = $( "#gender" ).val();
+        var result = akan(y, m, d, g); 
         alert("Your akan name is: " + result);
         //the .reset is for resetting the form after onsubmit
         document.getElementById("form").reset();
